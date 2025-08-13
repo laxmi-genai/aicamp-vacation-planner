672ea51b3534885d5366081b1fbaf520709cd386
Laxmi Harikumar <laxmih@google.com>
2025-08-13 16:27:58 -0700

    v1.0

---
+++ vacation-planner-v2/agent_engine/delete_agent_engine.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import vertexai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = vertexai.Client(
+    project = os.getenv("GOOGLE_CLOUD_PROJECT")
+    location = os.getenv("GOOGLE_CLOUD_LOCATION")
+)
+
+
+agent_engine = client.agent_engines.get(name=os.getenv("AGENT_ENGINE_NAME"))
+
+delete_engine = True
+
+if delete_engine:
+    agent_engine.delete(resource_name=agent_engine.resource_name, force=True)
